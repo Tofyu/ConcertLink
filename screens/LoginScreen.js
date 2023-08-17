6d6457a645ba0442f9d080a667586ebbbb3414e4
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-08-16 20:55:30 -0700

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,21 +1,53 @@
 import React, { useState, useEffect, useContext } from "react";
 import { StyleSheet, Text, View, ImageBackground  } from "react-native";
-import { Button, Input } from "@rneui/themed";
 import { auth, db } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { TextInput, Button } from 'react-native-paper';
 
 const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   
 
+  //Login function
+  const login = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        navigation.navigate("User BottomTab")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+    }
   return (
-    <View>
-      <View>
-        <Button 
-        title="Login" 
-        onPress={() => navigation.navigate("User BottomTab")} />
+    
+    <View style={{marginTop:230}}> 
+      <TextInput
+        placeholder="Enter your email"
+        leftIcon={{ type: "material", name: "email" }}
+        styles={styles}
+        label="Email"
+        onChangeText={setEmail}
+      />
 
-        <Button 
-        title="Create Account" 
-        onPress={() => navigation.navigate("Volunteer Register")} />
+      <TextInput
+        placeholder="Enter your password"
+        leftIcon={{ type: "material", name: "lock" }}
+        style={styles}
+        label="Password"
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <View style={{alignItems:'center'}}>
+        <Button onPress={login} > Login </Button>
+        
+
+        <Button onPress={() => navigation.navigate("Register")} > Create Account </Button>
+        
       </View>
     </View>
     
