2c66a594d8e039b60212da61419543cb6891d0fd
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-08-24 23:05:45 -0700

    Update VolunteerEventsScreen.js

--- screens/VolunteerEventsScreen.js
+++ screens/VolunteerEventsScreen.js
@@ -1,115 +1,140 @@
-import { StyleSheet, Button, Text, TextInput, View, FlatList, SafeAreaView} from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { collection, getDocs, doc } from "firebase/firestore";
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import { collection, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
 import { db, auth } from '../firebase';
+import { Avatar, Card, IconButton, Button } from 'react-native-paper';
 
-const VolunteerEventsScreen = ( {navigation} ) => {
-    const [events, setEvents] = useState([])
-    
-    useEffect(()=>{
-      fetchData();
-      
-    }, [])
+const VolunteerEventsScreen = ({ navigation }) => {
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [pastEvents, setPastEvents] = useState([]);
+  const [volunteerUser, setVolunteerUser] = useState()
+  const user = auth.currentUser;
+
+  const getVolunteerGroupID = async () => {
+    const docRef = doc(db, 'volunteers', user.uid);
+    const docSnap = await getDoc(docRef);
+    let groupID = '';
+
+    if (docSnap.exists()) {
+      groupID = docSnap.data().volunteerGroupID;
+      setVolunteerUser(docSnap.data().name)
+    } else {
+      console.log('No such document!');
+    }
+
+    return groupID;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let eventsFromDB = [];
+    const groupID = await getVolunteerGroupID();
+    const eventsQuery = query(collection(db, 'events'), where('volunteerGroupID', '==', groupID));
+    const querySnapshot = await getDocs(eventsQuery);
+    const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   
-    const Item = ({ name }) => (
-      <View style={{ padding: 20 }}>
-        <Text>{name}</Text>
-      </View>
-    );
-    
-    const SectionHeader = ({ dateTime }) => (
-      <View style={{ backgroundColor: '#f2f2f2', padding: 10 }}>
-        <Text>{dateTime}</Text>
-      </View>
-    );
-
-
-    const fetchData = async () => {
-      let eventsFromDB = [];
-      const querySnapshot = await getDocs(collection(db, 'events'));
-      const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      console.log("+++++++++++++++++++++++++++++++++++", docsData);
-    
-      for (const docSnap of docsData) {
-        if (!docSnap.empty) {
-          const songsCollectionRef = collection(db, 'events', docSnap.id, 'songs');
-          const songsSnapshot = await getDocs(songsCollectionRef);
-          const songsData = songsSnapshot.docs.map((songDoc) => songDoc.data());
-    
-          eventsFromDB.push({ id: docSnap.id, ...docSnap, songsData });
-          console.log("Document:", docSnap);
-        } else {
-          console.log("No such document!");
-        }
+    for (const docSnap of docsData) {
+      if (docSnap.volunteerGroupID === groupID) {
+        const songsCollectionRef = collection(db, 'events', docSnap.id, 'songs');
+        const songsSnapshot = await getDocs(songsCollectionRef);
+        const songsData = songsSnapshot.docs.map((songDoc) => songDoc.data());
+  
+        eventsFromDB.push({ id: docSnap.id, ...docSnap, songsData });
       }
-    
-      console.log("Events:", eventsFromDB);
-        
-        console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%", eventsFromDB)
+    }
+  
+    const sortedData = eventsFromDB.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
+  
+    const currentDateTime = new Date();
+    const upcoming = [];
+    const past = [];
+  
+    for (const event of sortedData) {
+      const eventDateTime = event.dateTime.toDate();  
 
-          // Todo: group by date
-          // const groupedData = eventsFromDB.reduce((acc, cur) => {
-          //   let options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-          //   const dateTime = cur.dateTime.toDate().toLocaleString(undefined, options); // format to date only;
-          //   const item = { id: cur.id, name: cur.volunteerGroupName, time: cur.dateTime.toDate().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }), location: cur.communityName };
-          //   const group = acc.find(group => group.dateTime === dateTime);
-          //   if (!group) {
-          //     acc.push({ dateTime, data: [item] });
-          //   } else {
-          //     group.data.push(item);
-          //   }
-          //   return acc;
-          // }, []);
+      if (eventDateTime <= currentDateTime) {
+        past.push(event);
+      } else {
+        upcoming.push(event);
+      }
+    }
+  
+    const sortedUpcoming = upcoming.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
+    const sortedPast = past.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
+  
+    setUpcomingEvents(sortedUpcoming);
+    setPastEvents(sortedPast);
+  };
+  
 
-          // Sort by date
-        const sortedData = eventsFromDB.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime)); 
+  const renderItem = ({ item }) => {
+    const currentDateTime = new Date();
+    const eventDateTime = item.dateTime.toDate();
+    let buttonToRender;
 
-        console.log('3.2************', sortedData);
-        setEvents(sortedData);
-          
-        console.log('4 useEffect end')
-      };
-
-
-      const renderItem = ({ item }) => (
-        
-        <View style={styles.item}>
-          <Text style={styles.label}>{item.volunteerGroupName}</Text>
-          <Text style={styles.subLabel}>Date:</Text>
-          <Text style={styles.info}>{item.dateTime.toDate().toLocaleString([], { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}</Text>
-          <Text style={styles.subLabel}>Location:</Text>
-          <Text style={styles.info}>{item.communityName}</Text>
-          <Text style={styles.subLabel}>Songs:</Text>
-          <View style={styles.songsContainer}>
-        {item.songsData.map((song, index) => (
-          song.isSelected && (
-            <Text key={index} style={styles.songName}>{song.name}</Text>
-          )
-        ))}
-      </View>
-        <Button title="See feedback" onPress={() => navigation.navigate('Volunteer Feedback', {
-            eventID: item.id
-          })} />
-        
-        </View>
+    if (eventDateTime <= currentDateTime) {
+      buttonToRender = (
+        <Button onPress={() => navigation.navigate('Volunteer Feedback', { eventID: item.id })}>
+          View Feedback
+        </Button>
       );
-      
+    } else {
+      buttonToRender = (
+        <Button onPress={() => navigation.navigate('Volunteer Event Details', { eventID: item.id })}>
+          Event Details
+        </Button>
+      );
+    }
+
     return (
+      <Card>
+        <Card.Title
+          title={item.volunteerGroupName}
+          subtitle={eventDateTime.toLocaleString([], {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        />
+        <Card.Actions>{buttonToRender}</Card.Actions>
+      </Card>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Welcome Text */}
+      <Text style={styles.welcomeText}>Welcome, {volunteerUser}!</Text>
+
+      <Button onPress={() => navigation.navigate('Group Register')}>
+          Register Your Volunteer Group
+        </Button>
       
-      <View style={styles.container}>
-      <FlatList 
-        data={events}
-        keyExtractor={(item, index) => item.id + index}
+      {/* Upcoming Events */}
+      <FlatList
+        data={upcomingEvents}
+        keyExtractor={(item) => item.id}
         renderItem={renderItem}
         contentContainerStyle={styles.listContent}
+        ListHeaderComponent={<Button>Upcoming Events</Button>}
       />
-    </View>
       
-    )
-
-  }
-
-export default VolunteerEventsScreen
+      {/* Past Events */}
+      <FlatList
+        data={pastEvents}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContent}
+        ListHeaderComponent={<Button>Past Events</Button>}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -118,37 +143,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
   },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
   listContent: {
     paddingBottom: 20,
   },
-  item: {
-    backgroundColor: '#f2f2f2',
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subLabel: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: '#666',
-  },
-  info: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  songsContainer: {
-    marginTop: 5,
-  },
-  songName: {
-    fontSize: 14,
-    marginBottom: 3,
-    color: '#333',
-  },
+});
 
-})
+export default VolunteerEventsScreen;
