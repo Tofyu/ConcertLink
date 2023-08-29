50472839e139dae9ba979cba388db44170a1b794
youapotato <74153243+youapotato@users.noreply.github.com>
2023-08-28 21:52:30 -0700

    Update EventDetailsScreen.js

--- screens/EventDetailsScreen.js
+++ screens/EventDetailsScreen.js
@@ -1,96 +1,66 @@
-import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { collection, addDoc, getDoc, doc, getDocs, query, update } from "firebase/firestore"; 
-import { db } from '../firebase';
+import { Text, TextInput, Button, Card } from 'react-native-paper';
+import { db } from '../firebase'
 
-const EventDetailsScreen = ({ navigation, route }) => {
-  const [event, setEvent] = useState({});
-  const [songs, setSongs] = useState([]);
-  const [date, setDate] = useState('');
-  const [message, setMessage] = useState('');
-  const { eventID } = route.params;
+const EventDetailsScreen = ( { navigation, route } ) => {
+    const [event, setEvent] = useState({})
+    const [songs, setSongs] = useState([])
+    const [date, setDate] = useState('')
+    const [message, setMessage] = useState('')
+    const { eventID } = route.params;
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-  useEffect(() => {
-    console.log(event);
-  }, [event]);
+    useEffect(() => {
+        console.log(event)
+    }, [event])
 
-  useEffect(() => {
-    console.log(songs);
-  }, [songs]);
+    useEffect(() => {
+        console.log(songs)
+    }, [songs])
 
-  const fetchData = async () => {
-    const eventFromDB = await getDoc(doc(db, "events", eventID));
-    setEvent({ ...eventFromDB.data(), id: eventID });
+    const fetchData = async () => {
+        const eventFromDB = await getDoc(doc(db, "events", eventID))
+        setEvent({...eventFromDB.data(), id:eventID})
 
-    let songsFromDB = [];
-    const querySnapshot = await getDocs(query(collection(db, "events", eventID, "songs")));
-    querySnapshot.forEach((doc) => {
-      songsFromDB.push({ ...doc.data(), id: doc.id });
-    });
-    setSongs(songsFromDB);
-    
-    setDate(eventFromDB.data().dateTime.toDate().toDateString() + " at " + eventFromDB.data().dateTime.toDate().toLocaleTimeString());
-  };
+        let songsFromDB = []
+        const querySnapshot = await getDocs(query(collection(db, "events", eventID, "songs")))
+        querySnapshot.forEach((doc) => {
+            songsFromDB.push({...doc.data(), id:doc.id})
+        })
+        setSongs(songsFromDB)
+        
+        setDate(eventFromDB.data().dateTime.toDate().toDateString() + " at " + eventFromDB.data().dateTime.toDate().toLocaleTimeString())
+    }
 
-  const renderItem = ({ item }) => (
-    <View style={styles.songItem}>
-      <Text style={styles.songName}>Song: {item.name}</Text>
-    </View>
-  );
+    const renderItem = ({item}) => (
+        <Card>
+            <Card.Title title = {"Song: " + item.name} />
+        </Card>
+    )
 
-  const submit = async () => {
-    await db.collection('events').doc(eventID).update({ "notes": event.notes + '\n' + message });
-    fetchData();
-  };
+    const submit = async () => {
+        await db.collection('events').doc(eventID).update({"notes":event.notes + '\n' + message})
+        fetchData;
+    }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Event Details</Text>
-      <Text style={styles.label}>Name: {event.communityName}</Text>
-      <Text style={styles.label}>Date & Time: {date}</Text>
-      <Text style={styles.label}>Status: {event.status}</Text>
-      <FlatList data={songs} renderItem={renderItem} />
-      <TextInput
-        style={styles.input}
-        placeholder='Message'
-        onChangeText={setMessage}
-      />
-      <Button title="Send" onPress={submit} />
-    </View>
-  );
-};
+    return (
+        <SafeAreaView>
+            <Text variant = "headlineSmall">Event Details</Text>
+            <Text variant = "headlineSmall">name: {event.communityName}</Text>
+            <Text variant = "headlineSmall">dateTime: {date}</Text>
+            <Text variant = "headlineSmall">status: {event.status}</Text>
+            <FlatList data = {songs} renderItem = {renderItem}/>
+            <TextInput placeholder = 'Message' onChangeText = {setMessage} />
+            <Button mode = 'elevated' onPress = {submit}>Send</Button>
+        </SafeAreaView>
+    )
+}
 
-export default EventDetailsScreen;
+export default EventDetailsScreen
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  songItem: {
-    marginBottom: 8,
-  },
-  songName: {
-    fontSize: 16,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 8,
-    marginBottom: 16,
-  },
-});
+const styles = StyleSheet.create({})
